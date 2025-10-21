0948d58256c5838eb59d4fe7f9155178d0379772
Rita-mary Amakom <amakomritamary322@gmail.com>
2025-10-21 12:42:06 +0100

    nl filter fix

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -4,7 +4,6 @@ from .views import ListCreateAnalyzedStringsView, StringRetrieveDestroyView, Nat
 urlpatterns = [
     path('strings/', ListCreateAnalyzedStringsView.as_view(), name='list-create-strings-slash'),
     path('strings', ListCreateAnalyzedStringsView.as_view(), name='list-strings'),
-    path('strings/<str:value>', StringRetrieveDestroyView.as_view(), name='get-string'),
-    path('strings/<str:value>/', StringRetrieveDestroyView.as_view(), name='get-string'),
     path('strings/filter-by-natural-language', NaturalLanguageFilterView.as_view(), name='nl-filter'),
-]
+    path('strings/<str:value>', StringRetrieveDestroyView.as_view(), name='get-string'),
+]       
